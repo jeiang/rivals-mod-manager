58ed700f0786c5780e511782cf096fe6c15d6a2e
Aidan Pinard <aidan@aidanpinard.co>
2026-04-16 23:47:32 -0400

    chore: iced i guess

--- src/lib.rs
+++
@@ -1,9 +0,0 @@
-#![deny(clippy::all)]
-#![allow(dead_code)]
-
-mod app;
-mod categories;
-mod mods;
-mod settings;
-
-pub use app::App;
--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,9 @@
 #![deny(clippy::all)]
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
-#[tokio::main]
-async fn main() -> eframe::Result {
-    env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
+mod app;
 
-    let native_options = eframe::NativeOptions {
-        viewport: egui::ViewportBuilder::default()
-            .with_inner_size([400.0, 300.0])
-            .with_min_inner_size([300.0, 220.0])
-            .with_icon(
-                eframe::icon_data::from_png_bytes(&include_bytes!("./assets/logo.png")[..])
-                    .expect("Failed to load icon"),
-            ),
-        ..Default::default()
-    };
-    eframe::run_native(
-        "Rivals Mod Manager",
-        native_options,
-        Box::new(|cc| Ok(Box::new(rivals_mod_manager::App::new(cc)))),
-    )
+#[tokio::main]
+async fn main() -> iced::Result {
+    iced::run(update, view)
 }
